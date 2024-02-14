27724015db858270db84b224776887e622d4e1ff
milligang <milligan.grinstead@gmail.com>
2024-02-14 17:48:47 -0500

    good progress - can read the graph adn interpret the data

--- Notebooks/geodesic2.py
+++ Notebooks/geodesic2.py
@@ -2,9 +2,10 @@
 import glob
 import networkx as nx
 import numpy as np
+import matplotlib.pyplot as plt
 
 from custom_functions import *
-from my_custom_functions import geodesic_distancematrix
+from my_custom_functions import *
 
 '''
 Tasks:
@@ -15,78 +16,65 @@ for num in nums is not returning what I am looking for
 
 # network x graph for edge length calculations
 dataset = "STARE1"
-nefi_output_folder = "../Data/Dataset_1/NEFI_graphs/*/"
+nefi_output_folder = "../Data/Dataset_1/NEFI_graphs_VK/*/"
 
 file_name = "im"
 nefi_outputs = glob.glob(f"{nefi_output_folder}*.txt")
 
-nodes = []
-edges = []
+nodes_count_all = []
+edges_count_all = []
 edge_lengths = []
  
 # currently only working with graph 77
 #nums = np.array([1,2,3,4,5,44,77,81,82,139,162, 163, 235, 236, 239, 240, 255, 291, 319, 324])
 nums = np.array([77])
 
+# updates list of number of edges, number of nodes, and average edge length given list of graphs (nums)
 for num in nums:
-    
-    num_str = f"{str(num).zfill(4)}"
-    
+    num_str = f"{str(num).zfill(4)}" # returns a 4 digit integer, the ending digits are num and the remaining front digits are 0
     #find nefi output file
     nefi_output = [s for s in nefi_outputs if num_str in s]
     #ensure there is only one location in this list
     assert len(nefi_output)==1
     #read in graph
     xgraph_in = nx.read_multiline_adjlist(nefi_output[0],delimiter='|')
-    edges_tmp, nodes_tmp, edge_lengths_tmp = network_descriptors(xgraph_in)
-    edges.append(edges_tmp)
-    nodes.append(nodes_tmp)
-    # edge_lengths is a list of float lists, one float list per graph
-    edge_lengths.append(edge_lengths_tmp)
+    edges_count_single, nodes_count_single, edge_lengths_single = my_network_descriptors(xgraph_in)
+    edges_count_all.append(edges_count_single) # number of nodes for all the graphs
+    nodes_count_all.append(nodes_count_single) # number of edges for all the graphs
+    edge_lengths.append(edge_lengths_single) # list of (float list of the average length for each edge) for all the graphs
 
 # path to the unweighted graph I want to use as graph_in
 graph_path = '../Data/Dataset_1/NEFI_graphs_VK/webs_im0077_#0_11h10m58s/Graph Filtering_smooth_2_nodes_im0077.txt'
-
-# read data, returning edges and nodes
-def read_graph_data(file_path):
-    graph_in = nx.Graph()
-
-    with open(file_path, 'r') as file:
-        for line in file:
-            # Skip comment lines
-            if line.startswith('#'):
-                continue
-            
-            # Split the line into coordinates and attributes
-            parts = line.strip().split('|')
-            head_coordinates = tuple(map(int, parts[0].strip('()').split(',')))
-            attributes = eval(parts[1])  # Safely evaluate the attributes part
-
-            # Add the head node to the graph
-            head_node = str(head_coordinates)
-            graph_in.add_node(head_node)
-
-            # If the head node has edges, add them to the graph
-            if isinstance(attributes, int) and attributes > 0:
-                for _ in range(attributes):
-                    # Split the line to get the coordinates of the tail node
-                    tail_coordinates = tuple(map(int, file.readline().strip().split('|')[0].strip('()').split(',')))
-                    tail_node = str(tail_coordinates)
-                    graph_in.add_node(tail_node)  # Add the tail node to the graph
-                    graph_in.add_edge(head_node, tail_node)  # Add the edge between head and tail nodes
-
-    return graph_in
+# read in graph from path
 graph_in = read_graph_data(graph_path)
 
-# function for nodes
 weighted_graph = Graph_to_weighted(graph_in)
-nodeList = get_nodes(weighted_graph)
-
+node_list = get_nodes(weighted_graph)
+edge_list = graph_in.edges
 # edit nodeList so it is compatible with geodesic functions
-nodeList_tuples = [tuple(node) for node in nodeList]
+node_list_tuples = [tuple(node) for node in node_list]
+# edit edges_list:
+# parse string representations of node coordinates into tuples of integers
+parse_coordinate = lambda coord_str: tuple(map(int, coord_str.strip('()').split(', ')))
+#convert string representations to tuples of integers using list comprehension
+edge_list_ints = [(parse_coordinate(start), parse_coordinate(end)) for start, end in edge_list]
 
 # central node returns the coordinates of the central node 
 central_node = central_node_ID(weighted_graph)
 
 # call geodesic function
-geodesic_distancematrix(nodeList_tuples, edges, edge_lengths, central_node)
+shortest_path = geodesic_distancematrix(node_list_tuples, edge_list_ints, edge_lengths[0], central_node)
+
+# make networkx graph
+G = nx.Graph()
+G.add_nodes_from(node_list_tuples)
+for edgeID,edge in enumerate(edge_list_ints):
+    G.add_edge(edge_list_ints[0],edge_list_ints[1],weight=edge_lengths[0][edgeID])
+
+dict = {i: array for i, array in enumerate(node_list)}
+# create plot
+# print(np.min(np.array(edge_list_ints)))
+print(node_list_tuples)
+nx.draw(G, dict)
+plt.show()
+plt.draw(G)
--- Notebooks/my_custom_functions.py
+++ Notebooks/my_custom_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import networkx as nx
+import matplotlib
 
 def my_network_descriptors(graph_in):
     
@@ -46,22 +47,43 @@ def geodesic_distancematrix(nodeList, edgeList, edgeLength, centralNode):
 	edgeLength: list of length of each edge
 	centralNode: coordinates of the central node
 	"""
-	
 	Nnodes = len(nodeList) # the number of nodes
-	
 	# Create the graph structure required for networkx
 	G = nx.Graph()
 	G.add_nodes_from(nodeList)
-
 	for edgeID,edge in enumerate(edgeList):
 		G.add_edge(edgeList[0],edgeList[1],weight=edgeLength[edgeID])
+	# Find the shortest paths between nodes and source
+	shortest_path = nx.single_source_dijkstra_path_length(G, centralNode, cutoff = None, weight='weight')
 
-	# Find the shortest paths between nodes and source, and unpack in distance matrix
-	DistanceDict = dict(nx.single_source_dijkstra_path_length(G, centralNode))
-	DM = np.ones((Nnodes,Nnodes))*-1
-	for i in range(Nnodes):
-		for j in range(Nnodes):
-			DM[i,j] = DistanceDict[i][j]
-			
-	return DM
+	return shortest_path
 
+# read data, returning edges and nodes
+def read_graph_data(file_path):
+    graph_in = nx.Graph()
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            # Skip comment lines
+            if line.startswith('#'):
+                continue
+            
+            # Split the line into coordinates and attributes
+            parts = line.strip().split('|')
+            head_coordinates = tuple(map(int, parts[0].strip('()').split(',')))
+            attributes = eval(parts[1])  # Safely evaluate the attributes part
+
+            # Add the head node to the graph
+            head_node = str(head_coordinates)
+            graph_in.add_node(head_node)
+
+            # If the head node has edges, add them to the graph
+            if isinstance(attributes, int) and attributes > 0:
+                for _ in range(attributes):
+                    # Split the line to get the coordinates of the tail node
+                    tail_coordinates = tuple(map(int, file.readline().strip().split('|')[0].strip('()').split(',')))
+                    tail_node = str(tail_coordinates)
+                    graph_in.add_node(tail_node)  # Add the tail node to the graph
+                    graph_in.add_edge(head_node, tail_node)  # Add the edge between head and tail nodes
+
+    return graph_in
