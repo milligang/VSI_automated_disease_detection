5b9ff1cd3144f7623bec5686d94d3e18e7fa459b
milligang <milligan.grinstead@gmail.com>
2024-02-23 15:00:19 -0500

    wrote persistance diagram functions

--- My_Analysis/my_persistence.py
+++ My_Analysis/my_persistence.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 from operator import itemgetter
 
 # read in data as a networkx graph
@@ -85,6 +86,10 @@ central_node = my_central_node_ID(nxgraph) # this output matches the central nod
 geodesic_distance = nx.single_source_dijkstra_path_length(nxgraph, central_node, cutoff=None, weight='weight')
 
 # create functions to make persistence diagram
+# create the initial persistence diagram
+x_coords, y_coords = [], []
+plt.scatter(x_coords, y_coords, color='blue', label='Initial Points')
+
 # connected_nodes takes in a node and returns a list of all of the nodes connected to it (moving up the branch)
 def connected_nodes(n):
     tail_nodes = []
@@ -92,22 +97,33 @@ def connected_nodes(n):
         if head == n:
             tail_nodes.append(tail)
     return tail_nodes
-'''
-def graph(tuple) = 
-    add tuple to persistence diagram
 
-def compare(tuple_list) = 
-    input format = [(b1, d1); (b2, d2);...]
+# graph takes a list of birth, death pairs as input and adds these to the persistance diagram
+def graph(tuple_list):
+    xy = zip(*tuple_list)
+    x_coords.append(xy[0])
+    y_coords.append(xy[1])
+    plt.scatter(x_coords, y_coords, color='red', label='New Points')
+    plt.legend()
+
+# compare takes in a list of birth, death tuples for a single branch and returns the tuple with highest death value, adding the rest of the tuples to the persistence diagram
+def compare(tuple_list): 
     # find the tuple with the highest death value, the 'most persistent'
-    max_tuple = max(tuple_list, key = itemgetter(1))
-    fix: if multiple max_tuple, max is the first one
+    max_tuple = max(tuple_list, key = itemgetter(1)) # if multiple max_tuples, returns the first one
     tuple_list.remove(max_tuple)
-    graph(tuple)
+    max_tuple = max_tuple[0], max_tuple[1] + 1
+    graph(tuple_list)
+    return max_tuple
 
-def branches(n) =
-    connected_nodes = connected_nodes(n)
+# branches takes a node n as input and recursivley calls itself to return a birth, death tuple associated with the branch 
+def branches(n):
+    connections = connected_nodes(n)
+    # create a list to store the birth, death tuple of each node connected to n, including n itself
     persistence_values = [(geodesic_distance[n], 0)]
-    for connected_node in connected_nodes:
-            persistence_values.append(branches(connected_node))
+    # for each node connected to n, add branches(connection) to the list
+    for connection in connections:
+            persistence_values.append(branches(connection))
     return compare(persistence_values)
-'''
+
+branches(central_node)
+plt.show()
